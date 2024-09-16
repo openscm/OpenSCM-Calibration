957069c02b0788767839a6c150649e047154493c
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2024-09-16 14:29:42 +0200

    Split out emcee plotting functions into package

--- src/openscm_calibration/emcee_utils.py
+++ src/openscm_calibration/emcee_utils.py
@@ -4,9 +4,11 @@ Helpers for emcee
 
 from __future__ import annotations
 
+import warnings
 from typing import TYPE_CHECKING
 
 import numpy as np
+from attrs import define
 
 if TYPE_CHECKING:
     # See here for explanation of this pattern and why we don't need quotes
@@ -15,6 +17,85 @@ if TYPE_CHECKING:
 
     import emcee.backends
     import numpy.typing as nptype
+
+
+def get_start(
+    sampler: emcee.ensemble.EnsembleSampler,
+    start: nptype.NDArray[np.float64] | None = None,
+) -> nptype.NDArray[np.float64]:
+    """
+    Get starting point for emcee sampling
+
+    Parameters
+    ----------
+    sampler
+        Sampler which will do the sampling
+
+    start
+        Starting point to use.
+
+        This is only used if the sampler has not already performed some iterations.
+
+    Returns
+    -------
+    :
+        Starting point for the sampling
+
+    Raises
+    ------
+    TypeError
+        `start` is `None` and the sampler has not performed any iterations yet.
+
+    Warns
+    -----
+    UserWarning
+        `start` is not `None` but the sampler has already performed iterations.
+
+        In this case, we use the sampler's last iteration
+        and ignore the value of `start`.
+    """
+    if sampler.iteration < 1:
+        # Haven't used any samples yet, hence must have start
+        if start is None:
+            msg = (
+                "The sampler has not performed any iterations yet. "
+                "You must provide a value for `start`. "
+                f"Received {start=}."
+            )
+            raise TypeError(msg)
+
+        res = start
+
+    else:
+        # Sampler has already done iterations, hence ignore start
+        if start is not None:
+            # Avoidable user side-effect, hence warn
+            # (see https://docs.python.org/3/howto/logging.html#when-to-use-logging)
+            warn_msg = (
+                "The sampler has already performed iterations. "
+                "We will use its last sample as the starting point "
+                "rather than the provided value for `start`."
+                "(If you with to re-start the sampling, reset the sampler)."
+            )
+            warnings.warn(warn_msg)
+
+        res = sampler.get_last_sample()
+
+    return res
+
+
+@define
+class ChainProgressInfo:
+    """Information about the progress of MCMC chains"""
+
+    steps: int
+    """Number of steps in the chain"""
+
+    steps_post_burnin: int
+    """Number of steps after the chain's burn-in period"""
+
+    acceptance_fraction: float
+    """Acceptance fraction of proposed steps"""
 
 
 def get_acceptance_fractions(
@@ -48,13 +129,62 @@ def get_acceptance_fractions(
     return acceptance_fractions
 
 
+@define
+class AutoCorrelationInfo:
+    """Information about auto-correlation within an MCMC chain"""
+
+    steps_post_burnin: int
+    """The number of steps in the chains post burn-in."""
+
+    tau: nptype.NDArray[np.float64]
+    """
+    Auto-correlation for each parameter
+
+    I.e. the first value is the auto-correlation for the first parameter,
+    second value is the auto-correlation for the second parameter,
+    ...
+    nth value is the auto-correlation for the nth parameter,
+    ...
+    """
+
+    convergence_ratio: float
+    """
+    Convergence ratio used to assess convergence.
+
+    For details, see
+    [`get_autocorrelation_info`][openscm_calibration.emcee_utils.get_autocorrelation_info].
+    """
+
+    converged: tuple[bool, ...]
+    """
+    Whether, based on `convergence_ratio`, the chains for each parameter have converged
+
+    The first value is whether the chain for the first parameter has converged,
+    the second value is whether the chain for the second parameter has converged,
+    ...
+    nth value is whether the chain for the nth parameter has converged,
+    ...
+    """
+
+    def any_non_nan_tau(self) -> bool:
+        """
+        Check if any of the tau information is not nan
+
+        Returns
+        -------
+        :
+            Whether any of the tau values are non-nan
+        """
+        return np.any(np.logical_not(np.isnan(self.tau)))
+
+
 def get_autocorrelation_info(
     inp: emcee.backends.Backend,
     burnin: int,
     thin: int = 1,
     autocorr_tol: int = 0,
     convergence_ratio: float = 50,
-) -> dict[str, float | int | bool | nptype.NDArray[np.floating[Any]]]:
+) -> AutoCorrelationInfo:
     """
     Get info about autocorrelation in chains
 
@@ -67,44 +197,39 @@ def get_autocorrelation_info(
         Number of iterations to treat as burn-in
 
     thin
-        Thinning to apply to the chains. Emcee handles
-        this so that the returned output is in steps,
+        Thinning to apply to the chains.
+
+        Emcee handles this such that the returned output is in steps,
         not thinned steps.
 
     autocorr_tol
-        Tolerance for auto-correlation calculations. Set
-        to zero to force calculation to never fail
+        Tolerance for auto-correlation calculations.
+
+        Set to zero to force calculation to never fail
 
     convergence_ratio
-        If the number of iterations (excluding burn-in) is
-        greater than ``convergence_ratio`` multiplied by
-        the autocorrelation (averaged over all the chains),
-        we assume the chains have converged
+        Convergence ratio to apply when checking for convergence
+
+        If the number of iterations (excluding burn-in)
+        is greater than `convergence_ratio`
+        multiplied by the autocorrelation (averaged over all the chains),
+        we assume the chain has converged.
 
     Returns
     -------
-        Results of calculation, keys:
-
-        - "tau": autocorrelation in each chains
-        - "autocorr": average of tau
-        - "converged": whether the chains have converged or not based on
-          ``convergence_ratio``
-        - "convergence_ratio": value of ``convergence_ratio``
-        - "steps_post_burnin": Number of steps in chains post burn-in
-
+    :
+        Results of calculation
     """
     tau = inp.get_autocorr_time(discard=burnin, tol=autocorr_tol, thin=thin)
-    autocorr = np.mean(tau)
 
-    converged = inp.iteration - burnin > convergence_ratio * autocorr
+    converged = inp.iteration - burnin > convergence_ratio * tau
 
-    out = {
-        "tau": tau,
-        "autocorr": autocorr,
-        "converged": converged,
-        "convergence_ratio": convergence_ratio,
-        "steps_post_burnin": inp.iteration - burnin,
-    }
+    out = AutoCorrelationInfo(
+        steps_post_burnin=inp.iteration - burnin,
+        tau=tau,
+        convergence_ratio=convergence_ratio,
+        converged=converged,
+    )
 
     return out
 
@@ -125,11 +250,12 @@ def get_labelled_chain_data(
         Object from which to plot the state
 
     parameter_order
-        Order of model parameters. This must match the order used by  ``inp``.
+        Order of model parameters. This must match the order used by  `inp`.
 
     neg_log_likelihood_name
-        Name to use for the negative log likelihood data. If not provided,
-        negative log likelihood information is not returned.
+        Name to use for the negative log likelihood data.
+
+        If not provided, negative log likelihood information is not returned.
 
     burnin
         Number of iterations to treat as burn in
@@ -139,8 +265,9 @@ def get_labelled_chain_data(
 
     Returns
     -------
-        Chain data, labelled with parameter names and, if requested,
-        ``neg_log_likelihood_name``
+    :
+        Chain data, labelled with parameter names
+        and, if requested, `neg_log_likelihood_name`
     """
     all_samples = inp.get_chain(discard=burnin, thin=thin)
 
